f62930e360db76b16a123501616b3600a09983e2
Cross Nastasi <cross@dill.moe>
2023-09-18 19:29:00 -0400

    major changes

--- generate.py
+++ generate.py
@@ -1,95 +1,113 @@
 import torch
-from torch.utils.data import DataLoader
-import json
+import argparse
+from pathlib import Path
 import torch.nn as nn
-device = "cuda"
+import torch.nn.functional as F
+
 class GRULanguageModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers):
         super(GRULanguageModel, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.gru = nn.GRU(embedding_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, vocab_size)
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_dim, self.num_layers, batch_first=True)
+        self.fc = nn.Linear(self.hidden_dim, self.vocab_size)
+        self.char_to_idx = {}
+        self.idx_to_char = {}
 
     def forward(self, x):
         embedded = self.embedding(x)
         out, _ = self.gru(embedded)
-        out = self.fc(out)
-        return out
+        return self.fc(out)
 
-    def generate_text(self, start_text, max_length=200, device="cuda"):
-        generated_text = start_text
+    def set_char_mappings(self, char_to_idx, idx_to_char):
+        self.char_to_idx = char_to_idx
+        self.idx_to_char = idx_to_char
+
+    def generate_text(self, start_text, max_length=200, device="cuda", temperature=1.0):
         input_text = start_text
+        response_text = ""
 
         with torch.no_grad():
             for _ in range(max_length):
-                input_tensor = torch.tensor([[char_to_idx.get(char, 0) for char in input_text]], dtype=torch.long).to(device)
-
-                predictions = self(input_tensor)
-                predictions = predictions[:, -1, :].squeeze()
-
-                predicted_idx = torch.argmax(predictions, dim=-1).item()
+                input_tensor = torch.tensor([[self.char_to_idx.get(char, 0) for char in input_text]], dtype=torch.long).to(device)
+                logits = self(input_tensor)
+                logits = logits[:, -1, :] / temperature  # Adjust logits based on temperature
+                probabilities = F.softmax(logits, dim=-1).squeeze()
+                predicted_idx = torch.multinomial(probabilities, 1).item()  # Sample from the adjusted distribution
                 predicted_char = self.idx_to_char[predicted_idx]
+                print(predicted_char, end="", flush=True)  # Stream the character to the terminal
+                response_text += str(predicted_char)
+                input_text = input_text[1:] + predicted_char  # Slide the window for next input
 
-                generated_text += str(predicted_char)
-                input_text = generated_text[-self.seq_length:]
-
-        return generated_text
-
-# Load the vocabulary
-with open("vocab.json", "r") as infile:
-    char_to_idx = json.load(infile)
-
-idx_to_char = {idx: char for char, idx in char_to_idx.items()}
-vocab_size = len(char_to_idx)
-
-# Create a new model with the same architecture
-model = GRULanguageModel(vocab_size, embedding_dim=512, hidden_dim=2048, num_layers=6)
-
-# Load the checkpoint weights into the model
-checkpoint = torch.load("checkpoint_step_6000.pth", map_location=torch.device("cuda"))
-model.load_state_dict(checkpoint['model_state_dict'])
-model.to(device)
-model.eval()
-
-# Function to generate text
-def generate_text(prompt, max_length=200, temperature=1.0):
-    input_text = prompt
-    generated_text = ""  # Initialize generated_text as an empty string
-
-    with torch.no_grad():
-        for _ in range(max_length):
-            input_tensor = torch.tensor([[char_to_idx.get(char, 0) for char in input_text]], dtype=torch.long).to(device)
-
-            predictions = model(input_tensor)
-            predictions = predictions[:, -1, :].squeeze() / temperature  # Adjust for temperature
-
-            # Apply softmax to the scaled predictions
-            predicted_probs = torch.softmax(predictions, dim=-1)
-
-            # Sample from the probability distribution
-            predicted_idx = torch.multinomial(predicted_probs, 1).item()
-            predicted_char = idx_to_char[predicted_idx]
-
-            generated_text += str(predicted_char)
-            input_text += str(predicted_char)  # Update the input text with the predicted character
-
-            # Print the character as it's generated (flush=True ensures it's printed immediately)
-            print(predicted_char, end='', flush=True)
-
-            if predicted_char == '\n':
-                break  # Stop if the model predicts a newline character
-
-    print()  # Print a newline after generating the text
-
-# Chat with the model
-print("GRU Language Model Chat (Type 'exit' to quit)")
-
-while True:
-    user_input = input("You: ")
+        return response_text
+        
+def save_checkpoint(epoch, model, optimizer, filename):
+    checkpoint = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'char_to_idx': model.char_to_idx,
+        'idx_to_char': model.idx_to_char
+    }
+    torch.save(checkpoint, filename)
     
-    if user_input.lower() == 'exit':
-        break
+def load_checkpoint(filename, device):
+    checkpoint = torch.load(filename, map_location=device)
+    
+    # Extract embedded model parameters
+    vocab_size = checkpoint['vocab_size']
+    embedding_dim = checkpoint['embedding_dim']
+    hidden_dim = checkpoint['hidden_dim']
+    num_layers = checkpoint['num_layers']
+    
+    # Recreate the model using the embedded parameters
+    model = GRULanguageModel(vocab_size, embedding_dim, hidden_dim, num_layers).to(device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    
+    # Load character mappings if they were saved in the checkpoint
+    if 'char_to_idx' in checkpoint and 'idx_to_char' in checkpoint:
+        model.set_char_mappings(checkpoint['char_to_idx'], checkpoint['idx_to_char'])
+    
+    optimizer_state_dict = checkpoint.get('optimizer_state_dict', None)
+    
+    return model, optimizer_state_dict, checkpoint['epoch']
 
-    print("Model:", end='', flush=True)
-    response = generate_text(user_input, temperature=0.6)
-    print()
+def chat_with_model(model, device, temperature=1.0):
+    model.eval()
+    model.to(device)
+
+    print("You can now chat with the model. Type 'exit' to end the conversation.")
+    
+    while True:
+        start_text = input("You: ")
+        
+        # Check for exit command
+        if start_text.strip().lower() == 'exit':
+            print("Ending conversation. Goodbye!")
+            break
+
+        generated_text = model.generate_text(start_text, device=device, temperature=temperature)
+        print(f"Model: {generated_text}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint_path', type=Path, required=True, help="Path to the trained model checkpoint")
+    parser.add_argument('--temp', type=float, default=1.0, help='Temperature for text generation. Higher values make output more random, lower values make it more deterministic.')
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Create the model instance
+    model = GRULanguageModel(1, 1, 1, 1).to(device)  # Dummy values, will be overridden by checkpoint
+
+    # Load the checkpoint
+    model, _, _ = load_checkpoint(args.checkpoint_path, device)
+
+    chat_with_model(model, device)
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,26 +1,38 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, random_split, DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.tensorboard import SummaryWriter
 import json
-import tensorboard
 import subprocess
 import argparse
+
+
+# ====================== Model ======================
 
 class GRULanguageModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers):
         super(GRULanguageModel, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.gru = nn.GRU(embedding_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, vocab_size)
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.embedding = nn.Embedding(self.vocab_size, self.embedding_dim)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_dim, self.num_layers, batch_first=True)
+        self.fc = nn.Linear(self.hidden_dim, self.vocab_size)
+        self.char_to_idx = {}
+        self.idx_to_char = {}
 
     def forward(self, x):
         embedded = self.embedding(x)
         out, _ = self.gru(embedded)
-        out = self.fc(out)
-        return out
+        return self.fc(out)
+
+    def set_char_mappings(self, char_to_idx, idx_to_char):
+        self.char_to_idx = char_to_idx
+        self.idx_to_char = idx_to_char
 
     def generate_text(self, start_text, max_length=200, device="cuda"):
         generated_text = start_text
@@ -29,31 +41,30 @@ class GRULanguageModel(nn.Module):
         with torch.no_grad():
             for _ in range(max_length):
                 input_tensor = torch.tensor([[self.char_to_idx.get(char, 0) for char in input_text]], dtype=torch.long).to(device)
-
                 predictions = self(input_tensor)
                 predictions = predictions[:, -1, :].squeeze()
-
                 predicted_idx = torch.argmax(predictions, dim=-1).item()
                 predicted_char = self.idx_to_char[predicted_idx]
-
                 generated_text += str(predicted_char)
-                input_text = generated_text[-self.seq_length:]
-
+                input_text = generated_text[-len(input_text):]  # Use the recent sequence length
         return generated_text
+
+
+# ====================== Dataset ======================
 
 class NextCharPredictionDataset(Dataset):
     def __init__(self, text, seq_length):
         self.seq_length = seq_length
         self.text = text
-        self.chars = list(set(text))  # Collect unique characters in the text
-        self.chars.sort()  # Sort the characters alphabetically
+        self.chars = sorted(list(set(text)))
         self.vocab_size = len(self.chars)
         self.char_to_idx = {char: idx for idx, char in enumerate(self.chars)}
         self.idx_to_char = {idx: char for idx, char in enumerate(self.chars)}
 
         self.input_sequences = []
         self.target_sequences = []
-        for i in range(0, len(text) - seq_length, seq_length):
+
+        for i in range(0, len(text) - seq_length):
             input_seq = text[i:i + seq_length]
             target_seq = text[i + 1:i + seq_length + 1]
             input_tensor = torch.tensor([self.char_to_idx[char] for char in input_seq])
@@ -66,111 +77,201 @@ class NextCharPredictionDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.input_sequences[idx], self.target_sequences[idx]
+    
+    def get_all_data(self):
+        inputs = []
+        targets = []
+        for i in range(len(self)):
+            input_seq, target_char = self[i]
+            inputs.append(input_seq)
+            targets.append(target_char)
+        return torch.stack(inputs), torch.stack(targets)
+
+
+# ====================== Utility Functions ======================
 
 def save_checkpoint(epoch, model, optimizer, filename):
-    checkpoint = {
+    torch.save({
         'epoch': epoch,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
-    }
-    torch.save(checkpoint, filename)
+        'vocab_size': model.vocab_size,
+        'embedding_dim': model.embedding_dim,
+        'hidden_dim': model.hidden_dim,
+        'num_layers': model.num_layers,
+        'char_to_idx': model.char_to_idx,
+        'idx_to_char': model.idx_to_char,
+        # Add any other hyperparameters or training stats you want to save
+    }, filename)
 
-def train_gru(model, dataloader, optimizer, criterion, scheduler, num_epochs, save_interval, device, log_dir, start_epoch=0):
-    model = model.to(device)
-    writer = SummaryWriter(log_dir)
-    
-    step_count = 0  # Initialize a step count variable
 
-    for epoch in range(start_epoch, num_epochs):
-        total_loss = 0.0
+def load_checkpoint(filepath, model, optimizer):
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    model.set_char_mappings(checkpoint['char_to_idx'], checkpoint['idx_to_char'])
+    return model, optimizer, epoch
 
-        for step, (batch_input, batch_target) in enumerate(dataloader):
-            step_count += 1  # Increment the step count
+
+def train_epoch(model, dataloader, optimizer, criterion, device, writer, global_step):
+    model.train()
+    total_loss = 0.0
+    for step, (batch_input, batch_target) in enumerate(dataloader):
+        batch_input, batch_target = batch_input.to(device), batch_target.to(device)
+        optimizer.zero_grad()
+
+        output = model(batch_input)
+        output = output.view(-1, len(model.char_to_idx))
+        batch_target = batch_target.view(-1)
+
+        loss = criterion(output, batch_target)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+        writer.add_scalar('Training Loss', loss.item(), global_step)
+        global_step += 1
+
+    return total_loss / len(dataloader), global_step
+
+
+def validate_epoch(model, dataloader, criterion, device):
+    model.eval()
+    total_loss = 0.0
+
+    with torch.no_grad():
+        for batch_input, batch_target in dataloader:
             batch_input, batch_target = batch_input.to(device), batch_target.to(device)
-            optimizer.zero_grad()
 
-            # Forward pass through the model
             output = model(batch_input)
-
-            # Reshape the output and target sequences to compute the loss
-            output = output.view(-1, vocab_size)
+            output = output.view(-1, len(model.char_to_idx))
             batch_target = batch_target.view(-1)
 
             loss = criterion(output, batch_target)
-            loss.backward()
-            optimizer.step()
             total_loss += loss.item()
 
-            # Log loss per batch (step)
-            writer.add_scalar('Loss', loss.item(), global_step=step_count)  # Use step_count for global_step
+    return total_loss / len(dataloader)
 
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{step+1}/{len(dataloader)}], Loss: {loss.item()}')
+def main_training_loop(model, train_dataset, val_dataset, optimizer, criterion, scheduler, device, config):
+    # Create DataLoaders for training and validation datasets
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=False)
+    
+    writer = SummaryWriter(config['log_dir'])
+    global_step = 0
 
-            # Check if it's time to save a checkpoint
-            if step_count % save_interval == 0:
-                save_checkpoint(epoch, model, optimizer, f"checkpoint_step_{step_count}.pth")
+    for epoch in range(config['start_epoch'], config['num_epochs']):
+        # Train
+        model.train()
+        train_losses = []
+        for inputs_batch, targets_batch in train_dataloader:
+            inputs_batch, targets_batch = inputs_batch.to(device), targets_batch.to(device)
+            
+            optimizer.zero_grad()
+            outputs = model(inputs_batch)
+            loss = criterion(outputs.view(-1, outputs.size(-1)), targets_batch.view(-1))
+            loss.backward()
+            optimizer.step()
+            
+            train_losses.append(loss.item())
+            writer.add_scalar('Training Loss', loss.item(), global_step)
+            global_step += 1
+        
+        # Compute average training loss for the epoch
+        train_loss = sum(train_losses) / len(train_losses)
+        
+        # Validation
+        model.eval()
+        val_losses = []
+        with torch.no_grad():
+            for inputs_batch, targets_batch in val_dataloader:
+                inputs_batch, targets_batch = inputs_batch.to(device), targets_batch.to(device)
+                
+                outputs = model(inputs_batch)
+                loss = criterion(outputs.view(-1, outputs.size(-1)), targets_batch.view(-1))
+                val_losses.append(loss.item())
+        
+        # Compute average validation loss for the epoch
+        val_loss = sum(val_losses) / len(val_losses)
 
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss / len(dataloader)}')
-        scheduler.step(total_loss / len(dataloader))
+        print(f"Epoch [{epoch+1}/{config['num_epochs']}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
 
-    save_checkpoint(num_epochs - 1, model, optimizer, "final_model.pth")
+        scheduler.step(val_loss)
+        writer.add_scalar('Validation Loss', val_loss, global_step)
+        
+        if (epoch + 1) % config['save_interval'] == 0:
+            save_checkpoint(epoch, model, optimizer, f"checkpoint_epoch_{epoch}.pth")
+
+    save_checkpoint(config['num_epochs'], model, optimizer, "final_model.pth")
     writer.close()
 
-def create_model_and_optimizer(vocab_size, hyperparameters):
-    model = GRULanguageModel(vocab_size, **hyperparameters['model'])
-    optimizer = optim.Adam(model.parameters(), **hyperparameters['optimizer'])
-    return model, optimizer
+# ====================== Configuration and Main Script ======================
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='GRU Language Model Training')
-    parser.add_argument('--checkpoint', type=str, default=None, help='Path to checkpoint file to resume training')
-    parser.add_argument('--text_file', type=str, default=None, help='Path to the text file for training')
-    args = parser.parse_args()
-
-    log_dir = "./logs"
-    num_epochs = 1000
-    seq_length = 2048
-    subprocess.Popen(["tensorboard", "--logdir", log_dir])
-
-    # Read text from a file
-    if args.text_file is not None:
+def get_config(args):
+    if args.text_file:
         with open(args.text_file, 'r', encoding='utf-8') as file:
             text = file.read().replace('\n', '')
     else:
         text = "the quick brown fox jumps over the lazy dog the quick brown fox"
 
-    dataset = NextCharPredictionDataset(text, seq_length)
-    vocab_size = dataset.vocab_size
+    avg_word_length = len(text) / len(text.split())
+    seq_length = int(4 * avg_word_length)  # Roughly 4 times the average word length
 
-    # Save the vocab file immediately
-    with open("vocab.json", "w") as outfile:
-        json.dump(dataset.char_to_idx, outfile)
-
-    hyperparameters = {
-        'model': {
-            'embedding_dim': 512,
-            'hidden_dim': 2048,
-            'num_layers': 6,
-        },
-        'optimizer': {
-            'lr': 0.0004,
-        },
-        'save_interval': 2000,  # Set the save interval as a hyperparameter
+    config = {
+        'text': text,
+        'seq_length': seq_length,
+        'embedding_dim': args.embedding_dim or 128,
+        'hidden_dim': args.hidden_dim or 256,
+        'num_layers': args.num_layers or 2,
+        'batch_size': args.batch_size or 64,
+        'num_epochs': args.num_epochs or 30,
+        'lr': args.lr or 0.001,
+        'log_dir': args.log_dir or './logs',
+        'save_interval': args.save_interval or 10,
+        'start_epoch': 0
     }
+    return config
 
-    if args.checkpoint is not None:
-        # If a checkpoint path is provided, load the model and optimizer state from it
-        checkpoint = torch.load(args.checkpoint)
-        model, optimizer = create_model_and_optimizer(vocab_size, hyperparameters)
+
+def main():
+    parser = argparse.ArgumentParser(description='Train the language model.')
+    parser.add_argument('--text_file', type=str, help="Path to the input text file")
+    parser.add_argument('--embedding_dim', type=int, help="Size of the word embeddings")
+    parser.add_argument('--hidden_dim', type=int, help="Size of the GRU hidden layer")
+    parser.add_argument('--num_layers', type=int, help="Number of GRU layers")
+    parser.add_argument('--batch_size', type=int, help="Batch size for training")
+    parser.add_argument('--num_epochs', type=int, help="Number of epochs to train")
+    parser.add_argument('--lr', type=float, help="Learning rate")
+    parser.add_argument('--log_dir', type=str, help="Directory to save Tensorboard logs")
+    parser.add_argument('--save_interval', type=int, help="Epochs interval to save checkpoints")
+    parser.add_argument('--resume', type=str, help='Path to the checkpoint to resume training from.')
+
+    args = parser.parse_args()
+    config = get_config(args)
+    if args.resume:
+        checkpoint = torch.load(args.resume)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        start_epoch = checkpoint['epoch'] + 1
+        start_epoch = checkpoint['epoch'] + 1  # Start from the next epoch
+        # Load any other saved data as needed
     else:
-        # Otherwise, create a new model and optimizer
-        model, optimizer = create_model_and_optimizer(vocab_size, hyperparameters)
         start_epoch = 0
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    dataset = NextCharPredictionDataset(config['text'], config['seq_length'])
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    model = GRULanguageModel(dataset.vocab_size, config['embedding_dim'], config['hidden_dim'], config['num_layers']).to(device)
+    model.set_char_mappings(dataset.char_to_idx, dataset.idx_to_char)
+
+    optimizer = optim.Adam(model.parameters(), lr=config['lr'])
     criterion = nn.CrossEntropyLoss()
-    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, verbose=True)
+    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5)
 
-    train_gru(model, dataset, optimizer, criterion, scheduler, num_epochs, hyperparameters['save_interval'], "cuda", log_dir, start_epoch)
+    main_training_loop(model, train_dataset, val_dataset, optimizer, criterion, scheduler, device, config)
+
+if __name__ == "__main__":
+    main()
