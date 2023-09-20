7a07f5a5838f0d79e13ff362847a24b1c70e37b7
Cross Nastasi <cross@dill.moe>
2023-09-19 23:42:18 -0400

    adaptive contrastive decoding

--- chat.py
+++ chat.py
@@ -67,7 +67,29 @@ model = TransformerModel(
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
-def generate_with_contrastive_decoding_v2(input_tensor, model, strong_temperature, weak_temperature, output_length):
+def calculate_diversity(sequence):
+    """Calculate the diversity of the generated sequence as the ratio of unique tokens to total tokens."""
+    unique_tokens = len(set(sequence))
+    total_tokens = len(sequence)
+    return unique_tokens / total_tokens
+
+def adaptive_temperature_adjustment(sequence, temperature, min_temp, max_temp, target_diversity):
+    """Adjust the temperature based on the diversity of the generated sequence."""
+    diversity = calculate_diversity(sequence)
+    
+    # If diversity is lower than target, increase temperature
+    if diversity < target_diversity:
+        temperature *= 1.05
+    # If diversity is higher than target, decrease temperature
+    elif diversity > target_diversity:
+        temperature *= 0.95
+    
+    # Ensure temperature stays within bounds
+    temperature = max(min_temp, min(max_temp, temperature))
+    
+    return temperature
+
+def generate_with_adaptive_contrastive_decoding(input_tensor, model, strong_temperature, weak_temperature, output_length, min_temp, max_temp, target_diversity):
     char_indices = input_tensor[0].tolist()
     generated_sequence = []
 
@@ -95,13 +117,21 @@ def generate_with_contrastive_decoding_v2(input_tensor, model, strong_temperatur
         char_indices.append(char_idx)
         input_tensor = torch.tensor([char_indices])
 
+        # Adjust the strong temperature adaptively after each token generation
+        strong_temperature = adaptive_temperature_adjustment(generated_sequence, strong_temperature, min_temp, max_temp, target_diversity)
+
     return generated_sequence
 
-# Update the main loop to use Contrastive Decoding
-print("Chat with the model using Contrastive Decoding! Type 'exit' to end the chat.")
-strong_temperature = 0.5  # Original temperature
-weak_temperature = 1.5  # Higher temperature for the weak model
+# Update the main loop to use Adaptive Contrastive Decoding
+print("Chat with the model using Adaptive Contrastive Decoding! Type 'exit' to end the chat.")
+strong_temperature = 0.5  # Initial temperature for the strong model
+weak_temperature = 1.5  # Temperature for the weak model
 output_length = 200
+
+# Parameters for adaptive temperature adjustment
+min_temperature = 0.2  # Minimum bound for the temperature
+max_temperature = 1.5  # Maximum bound for the temperature
+target_diversity = 0.7  # Target ratio of unique tokens to total tokens
 
 while True:
     input_text = input("> ").lower()
@@ -111,8 +141,8 @@ while True:
     char_indices = [char_to_index(char, model) for char in input_text]
     input_tensor = torch.tensor([char_indices])
 
-    # Generate sequence using Contrastive Decoding
-    generated_sequence = generate_with_contrastive_decoding_v2(input_tensor, model, strong_temperature, weak_temperature, output_length)
+    # Generate sequence using Adaptive Contrastive Decoding
+    generated_sequence = generate_with_adaptive_contrastive_decoding(input_tensor, model, strong_temperature, weak_temperature, output_length, min_temperature, max_temperature, target_diversity)
 
     # Convert indices to characters and print
     output_chars = [index_to_char(idx, model) for idx in generated_sequence]
