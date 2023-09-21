d95aef29b708ab8e593b0a134edde43a76aae814
Cross Nastasi <cross@dill.moe>
2023-09-21 02:03:29 -0400

    xwin 1

--- train.py
+++ train.py
@@ -15,31 +15,35 @@ def compute_l2_norm(parameters):
 
 
 class TransformerModel(nn.Module):
-  def __init__(self, chars, embed_size, heads, num_layers, hidden_size, sequence_length, lr, epochs, checkpoint_interval, clip_value, weight_decay=0.001):
-    super(TransformerModel, self).__init__()
-    self.chars = chars
-    self.vocab_size = len(chars)
-    self.embed_size = embed_size
-    self.heads = heads
-    self.num_layers = num_layers
-    self.hidden_size = hidden_size
-    self.sequence_length = sequence_length
-    self.lr = lr
-    self.epochs = epochs
-    self.checkpoint_interval = checkpoint_interval
-    self.clip_value = clip_value
-    self.weight_decay = weight_decay
+    def __init__(self, chars, embed_size, heads, num_layers, hidden_size, sequence_length, lr, epochs, checkpoint_interval, clip_value, weight_decay=0.001):
+        super(TransformerModel, self).__init__()
+        self.chars = chars
+        self.vocab_size = len(chars)
+        self.embed_size = embed_size
+        self.heads = heads
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.sequence_length = sequence_length
+        self.lr = lr
+        self.epochs = epochs
+        self.checkpoint_interval = checkpoint_interval
+        self.clip_value = clip_value
+        self.weight_decay = weight_decay
 
-    self.embedding = nn.Embedding(vocab_size, embed_size)
-    self.transformer = nn.Transformer(embed_size, nhead=heads, num_encoder_layers=num_layers, num_decoder_layers=num_layers)
-    self.fc = nn.Linear(embed_size, vocab_size)
-    
-  def forward(self, x):
-    x = self.embedding(x)
-    x = self.transformer(x, x)
+        # Move the following line outside the forward function to reuse weights
+        self.embedding = nn.Embedding(vocab_size, embed_size)
 
-    x = self.fc(x)
-    return x
+        self.transformer = nn.Transformer(embed_size, nhead=heads, num_encoder_layers=num_layers, num_decoder_layers=num_layers)
+        self.fc = nn.Linear(embed_size, vocab_size)
+
+    def forward(self, x):
+        # Use the reused embedding matrix from outside the function
+        x = self.embedding(x)
+
+        x = self.transformer(x, x)
+
+        x = self.fc(x)
+        return x
 
 # Read and process data
 def read_data(file_path):
@@ -60,7 +64,7 @@ vocab_size = len(chars)
 # Hyperparameters
 embed_size = 2896
 hidden_size = 4096
-sequence_length = 1024
+sequence_length = 512
 heads = 8
 num_layers = 6
 lr = 0.0001
@@ -89,11 +93,11 @@ if os.path.exists(checkpoint_path):
 for epoch in range(epochs):
     total_steps = (len(text) - sequence_length) // sequence_length
     pbar = tqdm(range(0, len(text) - sequence_length, sequence_length), desc=f"Epoch {epoch+1}/{epochs}", total=total_steps)
-    
+
     for i in pbar:
         inputs = torch.tensor([char_to_index(c, chars) for c in text[i:i+sequence_length]], dtype=torch.long).to(device)
         targets = torch.tensor([char_to_index(c, chars) for c in text[i+1:i+1+sequence_length]], dtype=torch.long).to(device)
-    
+
         optimizer.zero_grad()
         outputs = model(inputs.unsqueeze(0))
         loss = criterion(outputs.squeeze(0), targets)
@@ -105,12 +109,12 @@ for epoch in range(epochs):
         torch.nn.utils.clip_grad_norm_(model.parameters(), model.clip_value)
 
         optimizer.step()
-        
+
         # Update the progress bar description to include the current loss
         pbar.set_description(f"Epoch {epoch+1}/{epochs} Loss: {loss.item():.4f}")
-        
-        # Save checkpoint at specified intervals
-        if (i // sequence_length) % checkpoint_interval == 0:
+
+        # Save checkpoint after the first epoch has finished
+        if (i // sequence_length) % checkpoint_interval == 0 and epoch > 0:  # Check if it's not the first epoch
             torch.save({
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
